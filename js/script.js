7c7a26a5b0b31a198f5824591c23430567209d97
LaytonAvery <laytonavery5@gmail.com>
2021-04-14 09:44:05 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $("#slides").superslides({
+    animation: "fade",
+    play: 4000,
+    pagination: false,
+  });
+
+  var typed = new Typed(".typed", {
+    strings: ["Software Engineer.", "Web Developer.", "Lifelong Learner."],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false,
+  });
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
+});
